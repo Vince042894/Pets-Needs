01a533c78ed3bbae0b51744b80b4699378063a4a
Vince042894 <carasketonlinejob@gmail.com>
2023-07-17 16:37:48 +0800

    Connection String

---
+++ Pets&Needs/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pets_Needs.Models
+{
+	public class Category
+	{
+		[Key]
+        public int Id { get; set; }                                          /// this a row and column name
+		[Required]
+		public string Name { get; set; }
+		public int DisplayOrder { get; set; }
+    }
+}
